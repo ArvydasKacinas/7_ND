ce97eadc9dda5b5e82dd325b535c26ce48f5d734
ArvydasKacinas <arvydaskacinas@gmail.com>
2022-07-04 21:42:17 +0300

    ciklas 4-5

---
+++ ciklas_3.js
@@ -0,0 +1,7 @@
+'use-script';
+
+for(let i=1; i<1000; i++) {
+    if (i%5===0) {
+        console.log(i);
+    }
+};
---
+++ ciklas_4.js
@@ -0,0 +1,7 @@
+'use-script';
+
+for(let i=1; i<1000; i++) {
+    if (i%2===0 && i%3===0) {
+        console.log(i);
+    }
+};
