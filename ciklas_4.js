a7dcf11b54b7e3252dde09eefa5381e1d0667c9f
ArvydasKacinas <arvydaskacinas@gmail.com>
2022-07-04 21:58:02 +0300

    pataisytas 4

--- ciklas_4.js
+++ ciklas_4.js
@@ -1,7 +1,14 @@
 'use-script';
 
-for(let i=1; i<1000; i++) {
+let a=200; //input variantas kai mygtuko paspaudimu gautume kiekis skaiciu ekrane
+let b=4000; //input variantas
+let kiekis=0;
+
+for(let i=a; i<b; i++) {
     if (i%2===0 && i%3===0) {
+        kiekis++
         console.log(i);
     }
 };
+
+console.log(kiekis);
