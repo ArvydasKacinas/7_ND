92d9c46cffe3ce315a487f799efb0a9703f32816
ArvydasKacinas <arvydaskacinas@gmail.com>
2022-07-04 21:20:16 +0300

    Index/ciklas 1 ir 2

---
+++ ciklas_1.js
@@ -0,0 +1,7 @@
+'use-script';
+
+for(let i=2; i<1000; i++) {
+    if (i%2===0) {
+        console.log(i);
+    }
+};
---
+++ ciklas_2.js
@@ -0,0 +1,7 @@
+'use-script';
+
+for(let i=1; i<1000; i++) {
+    if (i%2===1) {
+        console.log(i);
+    }
+};
