94dedbe5ea11bbde5fffbf831c2350e1bf2d92bc
ArvydasKacinas <arvydaskacinas@gmail.com>
2022-07-06 14:25:44 +0300

    masyvas 2

--- masyvas_2.js
+++ masyvas_2.js
@@ -1,4 +1,11 @@
 'use-script';
 
-let masyvas2=[];
+let nZenklisSkaicius=123456;
+let masyvas2=Array.from(String(nZenklisSkaicius), Number);
 
+
+console.log(masyvas2);
+
+masyvas2.reverse();
+
+console.log(masyvas2);
